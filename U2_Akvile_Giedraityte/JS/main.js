4651a7c60a48e3fda92a5629cb4e29f1874522c7
akvilina-vtmc <75795015+akvilina-vtmc@users.noreply.github.com>
2021-06-22 14:47:02 +0300

    Exam_2021_06_02
    All tasks.

---
+++ U2_Akvile_Giedraityte/JS/main.js
@@ -0,0 +1,7 @@
+let data = [
+    {"title" : "Java programuotojas", "text" : "Jaunesnysis Java programuotojas turi gebėti atlikti nesudėtingų informacinių sistemų, jų sudėtinių dalių kūrimo bei priežiūros darbus Java programavimo kalba, apimant išeities programinio kodo rašymą bei programinės įrangos konstravimą, duomenų saugojimą ir apdorojimą reliacinėse duomenų bazėse, programinės įrangos diegimą, konfigūravimą."},
+    {"title" : "JavaScript programuotojas", "text" : "Būsimasis JavaScript programuotojas mokysis projektuoti ir kurti vartotojo kompiuteriuose, išmaniuosiuose telefonuose, planšetėse ir kt. prieigos įrenginiuose naudojamą programinę įrangą (angl. front-end), parengtą jungtis prie serverių sistemų, duomenų bazių, valdyti programavimo aplinką ir kūrimo procesą."},
+    {"title" : "PHP programuotojas", "text" : "PHP programuotojas gebės projektuoti ir kurti pilnai funkcionuojančias informacines sistemas (angl. full stack), tam panaudojant nesudėtingas duomenų bazes, valdyti programavimo aplinką ir kūrimo procesą."}
+]
+
+
